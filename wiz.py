29d27a44f0196fd86347c46a81a613cabdd2d117
Daniele-Dondi <daniele.dondi@unipv.it>
2024-11-11 18:36:01 +0100

    modules rearrangment

---
+++ wiz.py
@@ -0,0 +1,12 @@
+import tkinter
+from tkinter import *
+from tkinter import messagebox
+from tkinter import filedialog
+from tkinter import simpledialog
+from tkinter import dialog
+from tkinter import ttk
+from modules.wizard import *
+
+base = Tk()
+StartWizard(base)
+base.mainloop()
